f6848f896d92671110871efc1016db803f873619
Scott Swingle <scott7swingle@gmail.com>
2019-12-07 21:54:16 -0800

    add solution to day 8

---
+++ day08.py
@@ -0,0 +1,15 @@
+import functools
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open("input.txt") as f:
+    img = np.fromiter(f.read().strip(), int).reshape(-1, 6, 25)
+
+# part 1
+print(min(((l == 0).sum(), (l == 1).sum() * (l == 2).sum()) for l in img)[1])
+
+# part 2
+decoded = functools.reduce(lambda a, b: (a < 2) * a + (a == 2) * b, img)
+plt.imshow(decoded)
+plt.show()
